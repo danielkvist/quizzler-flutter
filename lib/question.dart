9cf9e078da0ad4e09c2a087b20cf10a196064218
Danielkvist <danielkvist@protonmail.com>
2020-05-14 11:21:02 +0200

    add notes about Dart classes

--- lib/question.dart
+++ lib/question.dart
@@ -1,5 +1,57 @@
+// Dart classes
+// A class is basically a blueprint.
+// When we build an instance based on that
+// blueprint we have an Object.
+// A class have:
+//  * Properties (color, numberOfSeats, etc)
+//  * Methods (drive(), break(), etc)
+//
+// class Car {
+//   int numberOfDoors = 5;
+//
+//   void drive() {
+//     print('wheels start turning');
+//   }
+// }
+//
+// We can give to the properties on our Class
+// default values.
+//
+// To create an object we need
+//  * Create the class.
+//  * Create the object from the class.
+//
+// Car myCar = Car();
+// myCar.numberOfDoors = 7;
+// Dart uses the dot notation to acces values in our object.
+//
+// We can provide a constructor for our class:
+// class Human {
+//   double height;
+//
+//   Human({double h}) {
+//     height = h;
+//   }
+// }
+//
+// A method is a function defined inside a class:
+// class Human {
+//   double height;
+//
+//   Human({double h}) {
+//     height = h;
+//   }
+//
+//   void Talk(String whatToSay) {
+//      print(whatToSay);
+//   }
+// }
+//
+// Human jenny = Human(h: 174);
+// jenny.tal('Why is the sky blue?');
+
 class Question {
-  // State
+  // Properties
   String questionText;
   bool questionAnswer;
 
